98dc4bce189ecf2e0a2fc5e56f9fde43715f3133
Paweł Ulita <pawel@ulita.xyz>
2018-04-24 13:46:48 +0200

    Add some basic models: Poll, Vote, and VoteOption

--- vsdk/polls/models.py
+++ vsdk/polls/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from vsdk.service_development.models import KasaDakaUser
+
+
+class Poll(models.Model):
+    """
+    Polls in our system are represented by this model.
+    """
+    owner = models.ForeignKey(KasaDakaUser, on_delete=models.PROTECT, null=False)
+    start_date = models.DateTimeField(blank=False, null=False)
+    duration = models.DurationField(blank=False, null=False)
+
+
+class Vote(models.Model):
+    """
+    Votes are represented by this model.
+    """
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=False)
+    voter = models.ForeignKey(KasaDakaUser, on_delete=models.CASCADE, null=False)
+    vote = models.ForeignKey('VoteOption', on_delete=models.PROTECT, null=False)
+
+
+class VoteOption(models.Model):
+    """
+    Vote options are represented by this model.
+
+    TODO: Add some integration for actual phone numbers.
+    """
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, null=False)
+    value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(9)])
